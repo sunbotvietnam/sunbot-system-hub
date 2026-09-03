b63ed8d9620b8faf703d80114e9959c7a558bf93
sunbotvietnam <kiroteam2025@gmail.com>
2026-09-03 21:02:45 +0700

    Thêm dữ liệu dự phòng các trường Billing

---
+++ billing/fallback-data.js
@@ -0,0 +1,19 @@
+window.BILLING_SCHOOLS_FALLBACK=[
+{code:'VIETLAO',name:'Trường mầm non Việt Lào',address:'Số 97, Đ. Nguyễn Du, Phường Trường Vinh, Tỉnh Nghệ An, Việt Nam',mst:'2901045200',fullTuition:25000,fullService:20000,commission:20},
+{code:'BINHMINH',name:'Trường mầm non Bình Minh',address:'Khối 3, P. Quang Trung, Phường Thành Vinh, Tỉnh Nghệ An, Việt Nam',mst:'2901037400',fullTuition:25000,fullService:20000,commission:20},
+{code:'HHT',name:'Trường mầm non Hà Huy Tập',address:'Số 45, đường Phan Thái Ất, Phường Vĩnh Phú, Tỉnh Nghệ An, Việt Nam',mst:'2901034939',fullTuition:25000,fullService:20000,commission:20},
+{code:'HD2',name:'Trường mầm non Hưng Dũng 2',address:'B12, Đường Nguyễn Phong Sắc, Phường Trường Vinh, Tỉnh Nghệ An, Việt Nam',mst:'2901134877',fullTuition:25000,fullService:20000,commission:20},
+{code:'HOAHONG',name:'Trường mầm non Hoa Hồng',address:'Khu C, Phường Thành Vinh, Tỉnh Nghệ An, Việt Nam',mst:'2901037231',fullTuition:25000,fullService:20000,commission:20},
+{code:'DONGVINH',name:'Trường mầm non Đông Vĩnh',address:'Khối Vĩnh Thịnh, Phường Thành Vinh, Tỉnh Nghệ An, Việt Nam',mst:'2901035026',fullTuition:25000,fullService:20000,commission:20},
+{code:'QUANGTRUNG',name:'Trường mầm non Quang Trung',address:'Khối 1, Phường Thành Vinh, Tỉnh Nghệ An, Việt Nam',mst:'2901037344',fullTuition:25000,fullService:20000,commission:20},
+{code:'DOLUONGTT',name:'Trường Mầm non Thị trấn',address:'Xóm 5 Thị Trấn, xã Đô Lương, tỉnh Nghệ An, Việt Nam',mst:'2901520978',fullTuition:25000,fullService:20000,commission:20},
+{code:'YENSON',name:'Trường mầm non Yên Sơn',address:'Xóm Minh Hòa, Xã Đô Lương, Tỉnh Nghệ An, Việt Nam',mst:'2901524436',fullTuition:25000,fullService:20000,commission:20},
+{code:'HAD',name:'Trường mầm non Hoa Anh Dũng',address:'Cty TNHH Yên Lạc, số 7 ngõ 1, Nguyễn Trường Tộ, Phường Thành Vinh, Tỉnh Nghệ An, Việt Nam',mst:'2900548258',fullTuition:90000,fullService:20000,commission:20},
+{code:'UYNO',name:'Trường mầm non Uy Nỗ',address:'Đông Anh, Hà Nội',mst:'',fullTuition:100000,fullService:20000,commission:20},
+{code:'MAIDICH',name:'Trường mầm non Mai Dịch - Cầu Giấy',address:'243 Mai Dịch, Cầu Giấy, Hà Nội',mst:'',fullTuition:120000,fullService:40000,commission:20},
+{code:'QUYNHMAI',name:'Trường mầm non Quỳnh Mai',address:'Số 9 Phố Kim Ngưu, Phường Bạch Mai, Thành phố Hà Nội',mst:'',fullTuition:240000,fullService:60000,commission:20},
+{code:'MNB',name:'Trường mầm non B',address:'Số 5 Phan Chu Trinh, Phường Cửa Nam, Thành phố Hà Nội',mst:'',fullTuition:240000,fullService:60000,commission:20},
+{code:'TRANGAN',name:'Trường mầm non Tràng An',address:'Long Biên, Hà Nội',mst:'',fullTuition:120000,fullService:40000,commission:20},
+{code:'KIMCHUNG',name:'Trường mầm non Kim Chung',address:'Đông Anh, Hà Nội',mst:'',fullTuition:240000,fullService:60000,commission:20},
+{code:'HAVY',name:'Trường mầm non Hà Vỹ',address:'Đông Anh, Hà Nội',mst:'',fullTuition:120000,fullService:40000,commission:20}
+];
